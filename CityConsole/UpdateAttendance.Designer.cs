0bcdb4eecf626c3e321d91f7e3d87530d5dfe8d7
Brendon King <brendonking@outlook.com>
2020-01-02 14:39:04 -0700

    Resize attendance

--- CityConsole/UpdateAttendance.Designer.cs
+++ CityConsole/UpdateAttendance.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // UpdateAttendance
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(437, 281);
+            this.Name = "UpdateAttendance";
             this.Text = "UpdateAttendance";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
