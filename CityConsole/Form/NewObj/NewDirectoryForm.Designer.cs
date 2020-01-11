c42b5444ca9e78cfdd0852bbc5fc9424497045bd
Brendon King <brendonking@outlook.com>
2020-01-11 10:29:29 -0700

    Fixed element tags

--- CityConsole/Form/NewObj/NewDirectoryForm.Designer.cs
+++ CityConsole/Form/NewObj/NewDirectoryForm.Designer.cs
@@ -30,18 +30,18 @@
         {
             this.label3 = new System.Windows.Forms.Label();
             this.button_Submit = new System.Windows.Forms.Button();
-            this.textBox_Children = new System.Windows.Forms.TextBox();
+            this.textBox_Last = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox_Adults = new System.Windows.Forms.TextBox();
+            this.textBox_First = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox_Email = new System.Windows.Forms.TextBox();
+            this.dateTimePicker_Joined = new System.Windows.Forms.DateTimePicker();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker_Birthday = new System.Windows.Forms.DateTimePicker();
+            this.textBox_Address2 = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox_Address1 = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
@@ -65,13 +65,13 @@
             this.button_Submit.Text = "Submit";
             this.button_Submit.UseVisualStyleBackColor = true;
             // 
-            // textBox_Children
+            // textBox_Last
             // 
-            this.textBox_Children.Location = new System.Drawing.Point(76, 35);
-            this.textBox_Children.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox_Children.Name = "textBox_Children";
-            this.textBox_Children.Size = new System.Drawing.Size(200, 20);
-            this.textBox_Children.TabIndex = 1;
+            this.textBox_Last.Location = new System.Drawing.Point(76, 35);
+            this.textBox_Last.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_Last.Name = "textBox_Last";
+            this.textBox_Last.Size = new System.Drawing.Size(200, 20);
+            this.textBox_Last.TabIndex = 1;
             // 
             // label2
             // 
@@ -83,13 +83,13 @@
             this.label2.TabIndex = 13;
             this.label2.Text = "Last Name";
             // 
-            // textBox_Adults
+            // textBox_First
             // 
-            this.textBox_Adults.Location = new System.Drawing.Point(76, 11);
-            this.textBox_Adults.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox_Adults.Name = "textBox_Adults";
-            this.textBox_Adults.Size = new System.Drawing.Size(200, 20);
-            this.textBox_Adults.TabIndex = 0;
+            this.textBox_First.Location = new System.Drawing.Point(76, 11);
+            this.textBox_First.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_First.Name = "textBox_First";
+            this.textBox_First.Size = new System.Drawing.Size(200, 20);
+            this.textBox_First.TabIndex = 0;
             // 
             // label1
             // 
@@ -101,30 +101,30 @@
             this.label1.TabIndex = 10;
             this.label1.Text = "First Name";
             // 
-            // textBox2
+            // textBox_Email
             // 
-            this.textBox2.Location = new System.Drawing.Point(76, 59);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(200, 20);
-            this.textBox2.TabIndex = 2;
+            this.textBox_Email.Location = new System.Drawing.Point(76, 59);
+            this.textBox_Email.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_Email.Name = "textBox_Email";
+            this.textBox_Email.Size = new System.Drawing.Size(200, 20);
+            this.textBox_Email.TabIndex = 2;
             // 
-            // dateTimePicker1
+            // dateTimePicker_Joined
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(77, 132);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
-            this.dateTimePicker1.TabIndex = 5;
+            this.dateTimePicker_Joined.Location = new System.Drawing.Point(77, 132);
+            this.dateTimePicker_Joined.Name = "dateTimePicker_Joined";
+            this.dateTimePicker_Joined.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker_Joined.TabIndex = 5;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(28, 135);
+            this.label4.Location = new System.Drawing.Point(34, 135);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.Size = new System.Drawing.Size(38, 13);
             this.label4.TabIndex = 18;
-            this.label4.Text = "Birthday";
+            this.label4.Text = "Joined";
             // 
             // label5
             // 
@@ -136,20 +136,20 @@
             this.label5.TabIndex = 20;
             this.label5.Text = "Birthday";
             // 
-            // dateTimePicker2
+            // dateTimePicker_Birthday
             // 
-            this.dateTimePicker2.Location = new System.Drawing.Point(77, 158);
-            this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
-            this.dateTimePicker2.TabIndex = 6;
+            this.dateTimePicker_Birthday.Location = new System.Drawing.Point(77, 158);
+            this.dateTimePicker_Birthday.Name = "dateTimePicker_Birthday";
+            this.dateTimePicker_Birthday.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker_Birthday.TabIndex = 6;
             // 
-            // textBox1
+            // textBox_Address2
             // 
-            this.textBox1.Location = new System.Drawing.Point(76, 107);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(200, 20);
-            this.textBox1.TabIndex = 4;
+            this.textBox_Address2.Location = new System.Drawing.Point(76, 107);
+            this.textBox_Address2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_Address2.Name = "textBox_Address2";
+            this.textBox_Address2.Size = new System.Drawing.Size(200, 20);
+            this.textBox_Address2.TabIndex = 4;
             // 
             // label6
             // 
@@ -161,13 +161,13 @@
             this.label6.TabIndex = 22;
             this.label6.Text = "Address 2";
             // 
-            // textBox3
+            // textBox_Address1
             // 
-            this.textBox3.Location = new System.Drawing.Point(76, 83);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(200, 20);
-            this.textBox3.TabIndex = 3;
+            this.textBox_Address1.Location = new System.Drawing.Point(76, 83);
+            this.textBox_Address1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_Address1.Name = "textBox_Address1";
+            this.textBox_Address1.Size = new System.Drawing.Size(200, 20);
+            this.textBox_Address1.TabIndex = 3;
             // 
             // label7
             // 
@@ -184,20 +184,20 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(288, 219);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox_Address1);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox_Address2);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker_Birthday);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.dateTimePicker1);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.dateTimePicker_Joined);
+            this.Controls.Add(this.textBox_Email);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button_Submit);
-            this.Controls.Add(this.textBox_Children);
+            this.Controls.Add(this.textBox_Last);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox_Adults);
+            this.Controls.Add(this.textBox_First);
             this.Controls.Add(this.label1);
             this.Name = "NewDirectoryForm";
             this.Text = "New Member";
@@ -209,18 +209,18 @@
         #endregion
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button_Submit;
-        private System.Windows.Forms.TextBox textBox_Children;
+        private System.Windows.Forms.TextBox textBox_Last;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox_Adults;
+        private System.Windows.Forms.TextBox textBox_First;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox_Email;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_Joined;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.DateTimePicker dateTimePicker2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_Birthday;
+        private System.Windows.Forms.TextBox textBox_Address2;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox_Address1;
         private System.Windows.Forms.Label label7;
     }
 }
